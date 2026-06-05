089a16e2abee6adf0a289d9507c0477a9fa1e4f3
EricYin <public_2011_2011@qq.com>
2026-06-05 10:31:23 +0800

    Update fix_name.sh

--- fix_name.sh
+++ fix_name.sh
@@ -2,11 +2,7 @@
 
 # 1. 定义源字符串和目标字符串（查找时将忽略 source 的大小写）
 declare -A replace_pairs=(
-    ["xray "]="xcon "
-   # [" xray"]=" xcon"
-    ['"xray"']='"xcon"'
-    ["'xray'"]="'xcon'"
-    ["xray. "]="xcon. "
+    ["github.com/xtls/xray-core"]="github.com/ericyin/xray-core"
 )
 
 # 2. 指定扫描目录
